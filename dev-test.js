385a3e736bce6603b7d6170f4eadf43efbbdeb31
Renkon117 <randyok555@gmail.com>
2019-09-28 13:43:05 -0500

    created function mine block; enabled to create chain

--- dev-test.js
+++ dev-test.js
@@ -2,6 +2,11 @@
 
 const Block = require('./block');
 
-const block = new Block('foo', 'bar', 'zoo', 'baz');
-console.log(block.toString());
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+console.log(fooBlock.toString());
+
+
+// const block = new Block('foo', 'bar', 'zoo', 'baz');
+// console.log(block.toString());
+// console.log(Block.genesis().toString());
 
