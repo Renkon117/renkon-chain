0587a00ca36fe65ff3ca8a1df928a43a70a8a2a0
Renkon117 <randyok555@gmail.com>
2019-09-28 13:29:40 -0500

    created overall blockchain base project

---
+++ block.js
@@ -0,0 +1,20 @@
+
+
+class Block {
+    constructor(timestamp, lastHash, hash, data) {
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+    toString() {
+        return `Block -
+        TimeStamp: ${this.timestamp}
+        LastHash: ${this.lastHash.substring(0, 10)}
+        Hash: ${this.hash.substring(0, 10)}
+        Data: ${this.data}`;
+    }
+}
+
+module.exports = Block;
---
+++ dev-test.js
@@ -0,0 +1,7 @@
+//start with 'npm run dev-test'
+
+const Block = require('./block');
+
+const block = new Block('foo', 'bar', 'zoo', 'baz');
+console.log(block.toString());
+
