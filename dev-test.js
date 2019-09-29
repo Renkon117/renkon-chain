12eb8e5028ec980c860b02c1bd3b8f9248d97190
Renkon117 <randyok555@gmail.com>
2019-09-28 21:37:52 -0500

    organized files and added express library

--- dev-test.js
+++ dev-test.js
@@ -1,6 +1,6 @@
 //start with 'npm run dev-test'
 
-const Block = require('./block');
+const Block = require('./blockchain/block');
 
 const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
 console.log(fooBlock.toString());
