8355e28c62a1ac6803bdee3ecbcd73d1b73b05c9
Renkon117 <randyok555@gmail.com>
2019-09-28 20:07:17 -0500

    fixed typo error of valid function

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,9 +2,10 @@ const Blockchain = require('./blockchain');
 const Block = require('./block');
 
 describe('Blockchain', () => {
-    let bc;
+    let bc, bc2;
     beforeEach(() => {
         bc = new Blockchain();
+        bc2 = new Blockchain();
     });
 
     it('starts with the genesis block', () => {
@@ -15,5 +16,11 @@ describe('Blockchain', () => {
         const data = 'foo';
         bc.addBlock(data);
         expect(bc.chain[bc.chain.length - 1].data).toEqual(data);
+    });
+
+    it('validates a valid chain', () => {
+        bc2.addBlock('foo');
+
+        expect(bc.isValidChain(bc2.chain)).toBe(true);
     });
 });
