25d5a4359adc61e2f7070ded5d6266ab64263c16
Renkon117 <randyok555@gmail.com>
2019-09-29 16:05:10 -0500

    created transaction pool class

--- wallet/index.js
+++ wallet/index.js
@@ -1,5 +1,5 @@
 const ChainUtil = require('../chain-util');
-
+const Transaction = require('./transaction');
 const { INITIAL_BALANCE } = require('../config');
 
 class Wallet {
@@ -17,6 +17,24 @@ class Wallet {
 
     sign(dataHash) {
         return this.keyPair.sign(dataHash);
+    }
+
+    createTransaction(recipient, amount, transactionPool) {
+        if (amount > this.balance) {
+            console.log(`Amount: ${amount} exceeds current balance: ${this.balance}`);
+            return;
+        }
+
+        let transaction = transactionPool.exsistingTransaction(this.publicKey);
+
+        if (transaction) {
+            transaction.update(this, recipient, amount);
+        } else {
+            transaction = Transaction.new(this, recipient, amount);
+            transactionPool.updateOrAddTransaction(transaction);
+        }
+
+        return transaction;
     }
 }
 
