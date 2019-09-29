a5f8c8d8bcb5d5a75f2ca71e6919f74bb953c117
Renkon117 <randyok555@gmail.com>
2019-09-29 09:38:10 -0500

    added functionality of peer to peer

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -14,6 +14,8 @@ class P2pServer {
         server.on('connection', socket => this.connectSocket(socket));
 
         this.connectToPeers();
+
+        console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
     }
 
     connectToPeers() {
@@ -26,6 +28,17 @@ class P2pServer {
     connectSocket(socket) {
         this.sockets.push(socket);
         console.log('Socket connected');
+
+        this.messageHandler(socket);
+
+        socket.send(JSON.stringify(this.blockchain.chain));
+    }
+
+    messageHandler(socket) {
+        socket.on('message', message => {
+            const data = JSON.parse(message);
+            console.log('data', data);
+        });
     }
 }
 
