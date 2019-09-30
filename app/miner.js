2dc9a0f58dbb7b94fd186f9aa89310d326452c4b
Renkon117 <randyok555@gmail.com>
2019-09-29 20:24:47 -0500

    added clear transaction function on miner class

--- app/miner.js
+++ app/miner.js
@@ -1,3 +1,6 @@
+const Wallet = require('../wallet');
+const Transaction = require('../wallet/transaction');
+
 class Miner {
     constructor(blockchain, transactionPool, wallet, p2pServer) {
         this.blockchain = blockchain;
@@ -8,10 +11,12 @@ class Miner {
 
     mine() {
         const validTransactions = this.transactionPool.validTransactions();
-        //include a reward  for a miner
-        //create a block consisting of the valid transactions
-        //synchronize the chains in the peer-to-peer server
-        //clear the transaction pool
+        validTransactions.push(
+            Transaction.rewardTranasaction(this.wallet, Wallet.blockchain())
+        );
+        const block = this.blockchain.addBlock(validTransactions);
+        this.p2pServer.syncChains();
+        this.transactionPool.clear();
         //broadcast to every miner to clear their transaction pools
     }
 }
